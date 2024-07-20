7b062acecc70428f6a917e26ac994a60cdc8fe5f
Manish Borikar <manishborikar92@gmail.com>
2024-07-20 14:51:15 +0530

    Modified Backend

---
+++ api/predict.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const { exec } = require('child_process');
+const path = require('path');
+
+router.post('/predict', (req, res) => {
+    const imagePath = path.join(__dirname, '../public/uploads/', req.file.filename);
+    const command = `set PYTHONIOENCODING=utf-8 && python utils/preprocess.py "${imagePath}"`;
+
+    exec(command, { encoding: 'utf8' }, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error executing script: ${error}`);
+            return res.status(500).send('Internal Server Error');
+        }
+        // Trim any extra lines and parse the JSON
+        console.log('Python script output:', stdout);
+        try {
+            const jsonString = stdout.trim();
+            const result = JSON.parse(jsonString);
+            if (result.error) {
+                console.error(`Prediction error: ${result.error}`);
+                res.status(500).send('Internal Server Error');
+            } else {
+                res.render('result', { result });
+            }
+        } catch (parseError) {
+            console.error(`Error parsing JSON: ${parseError}`);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+});
+
+module.exports = router;
