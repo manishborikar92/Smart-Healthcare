c3f10d74137adb0308cb70602f39e0b8f8e969cb
Manish Borikar <manishborikar92@gmail.com>
2024-07-21 15:39:40 +0530

    PORT updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const multer = require('multer');
 const predictRouter = require('./api/predict');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
