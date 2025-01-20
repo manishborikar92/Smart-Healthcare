c70a4101fe9e596b8203261e6d79c48c7992e375
Manish Borikar <manishborikar92@gmail.com>
2025-01-20 15:33:58 +0530

    Uploaded model at huggingface. Updated code for huggingface.

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ const predictRouter = require('./api/predict');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Ensure uploads directory exists
+const uploadsDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadsDir)) {
+    fs.mkdirSync(uploadsDir); // Create the uploads directory if it doesn't exist
+}
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, uploadsDir); // Use the `uploads` directory
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+    cb(null, Date.now() + path.extname(file.originalname)); // Save with unique name
   }
 });
 const upload = multer({ storage: storage });
